bccae0f971c487365ef11e031c57b47b6fbdb3c7
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-03-12 14:03:59 +0000

    feat: implement Singleton pattern in Manager.java
    closes #43

--- mydrive/src/main/java/pt/tecnico/mydrive/domain/Manager.java
+++ mydrive/src/main/java/pt/tecnico/mydrive/domain/Manager.java
@@ -1,9 +1,25 @@
 package pt.tecnico.mydrive.domain;
 
+import pt.ist.fenixframework.FenixFramework;
+
+/*
+    Uses the Singleton pattern.
+*/
+
 public class Manager extends Manager_Base {
-    
-    public Manager() {
+
+    // TODO: not Reflection safe
+    public static Manager getInstance() {
+        Manager man = FenixFramework.getDomainRoot().getManager();
+        if (man == null) {
+            man = new Manager();
+        }
+        return man;
+    }
+
+    private Manager() {
         super();
+        FenixFramework.getDomainRoot().setManager(this);
     }
     
 }
