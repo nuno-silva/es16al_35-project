07a9a11e5cefcca6b92faa5fb5b15ada7808210e
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-03-13 23:35:33 +0000

    feat: fix build

--- mydrive/src/main/java/pt/tecnico/mydrive/domain/Manager.java
+++ mydrive/src/main/java/pt/tecnico/mydrive/domain/Manager.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class Manager extends Manager_Base {
 	private FileSystem currentFileSystem;
-	
+
     // TODO: not Reflection safe
     public static Manager getInstance() {
         Manager man = FenixFramework.getDomainRoot().getManager();
@@ -29,36 +29,36 @@ public class Manager extends Manager_Base {
         addFileSystems(newFs); //dont know how to set fileSystem...
         currentFileSystem = newFs;
     }
-    
+
     public ArrayList<File> showPathContent(String path) throws UnknownPathException {
-    	return currentFileSystem.pathContent(splitPath(path)); //FIXME 
+    	return currentFileSystem.pathContent(splitPath(path)); //FIXME
     }
-    
+
     public String showFileContent(String path) throws UnknownPathException {
     	return currentFileSystem.fileContent(splitPath(path)); //FIXME
     }
-    
+
     public void removePathContent(String path) throws UnknownPathException {
     	currentFileSystem.removeFile(splitPath(path)); //FIXME
     }
-    
+
     public void createUser(String username, String password, String name) throws InvalidUsernameException {
     	currentFileSystem.createUser(username, password, name);
     }
-    
+
     public ArrayList<String> splitPath(String path) throws UnknownPathException {
     	ArrayList<String> directories = new ArrayList<String>();
-    	
+
     	if(!path.substring(0, 1).matches("/")) //check if root directory is used, otherwise ERROR!
-    		throw new UnknownPathException();
-    	
+    		throw new UnknownPathException(path);
+
     	directories.add("/");
-    	
+
     	path = path.substring(1);
-    	
-    	for(String dir : path.split("/")) 
+
+    	for(String dir : path.split("/"))
     		directories.add(dir);
-    	
+
     	return directories;
-    } 
+    }
 }
