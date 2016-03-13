nuno-silva/es16al_35-project

commit f1ed5e1837281b17abfcef05ba8564edd362bf92

Manager and FileSystem first implementation. Please check FIXME's for non-existing function calls to other classes

mydrive/src/main/java/pt/tecnico/mydrive/domain/FileSystem.java
@@ -1,8 +1,9 @@
 package pt.tecnico.mydrive.domain;
+
 import org.jdom2.Element;
 import pt.tecnico.mydrive.xml.IXMLVisitable;
 import pt.tecnico.mydrive.xml.IXMLVisitor;
-
+import java.util.ArrayList;
 
 public class FileSystem extends FileSystem_Base implements IXMLVisitable {
 
@@ -16,6 +17,27 @@ public class FileSystem extends FileSystem_Base implements IXMLVisitable {
 
     private void init(String name) {
         setName(name);
+    }
+    
+    public ArrayList<File> pathContent (ArrayList<String> path) throws UnknownPathException {
+    	return splitPath(path).showContent(); //FIXME
+    }
+    
+    public String fileContent (ArrayList<String> path) throws UnknownPathException {
+    	return splitPath(path).getContent(); //FIXME
+    }
+    
+    public void removeFile (ArrayList<String> path) throws UnknownPathException {
+    	splitPath(path).deleteFile(); 
+    }
+    
+    public File splitPath(ArrayList<String> path) throws UnknownPathException {
+    	File currentDir = getRootDir();
+
+    	for(String dir : path) 
+    		currentDir = currentDir.getContent(dir); //FIXME
+    	
+    	return currentDir;
     }
 
     @Override

mydrive/src/main/java/pt/tecnico/mydrive/domain/Manager.java
@@ -1,12 +1,15 @@
 package pt.tecnico.mydrive.domain;
+
 import pt.ist.fenixframework.FenixFramework;
+import java.util.ArrayList;
 
 /*
     Uses the Singleton pattern.
 */
 
 public class Manager extends Manager_Base {
-
+	private FileSystem currentFileSystem;
+	
     // TODO: not Reflection safe
     public static Manager getInstance() {
         Manager man = FenixFramework.getDomainRoot().getManager();
@@ -19,6 +22,36 @@ public class Manager extends Manager_Base {
     private Manager() {
         super();
         FenixFramework.getDomainRoot().setManager(this);
+        
+        addFileSystems(new FileSystem()); //dont know how to set fileSystem...
     }
     
+    public ArrayList<File> showPathContent(String path) throws UnknownPathException {
+    	return currentFileSystem.pathContent(splitPath(path)); //FIXME 
+    }
+    
+    public String showFileContent(String path) throws UnknownPathException {
+    	return currentFileSystem.fileContent(splitPath(path)); //FIXME
+    }
+    
+    public void removePathContent(String path) throws UnknownPathException {
+    	currentFileSystem.removeFile(splitPath(path)); //FIXME
+    }
+    
+    
+    public ArrayList<String> splitPath(String path) throws UnknownPathException {
+    	ArrayList<String> directories = new ArrayList<String>();
+    	
+    	if(!path.substring(0, 1).matches("/")) //check if root directory is used, otherwise ERROR!
+    		throw new UnknownPathException();
+    	
+    	directories.add("/");
+    	
+    	path = path.substring(1);
+    	
+    	for(String dir : path.split("/")) 
+    		directories.add(dir);
+    	
+    	return directories;
+    } 
 }

mydrive/src/main/java/pt/tecnico/mydrive/domain/UnknownPathException.java
@@ -1,0 +1,7 @@
+package pt.tecnico.mydrive.domain;
+
+public class UnknownPathException extends Throwable {
+	public UnknownPathException() {
+		System.out.println("ERROR: Given path does not exist \n");
+	}
+}

commit 9295c008627c4306f02b1046218820b7cc837552

feat: fix build
* commit f1ed5e1837281b17abfcef05ba8564edd362bf92 broke the build. This commit
  fixes it by commenting out undefined methods.

affects #12

mydrive/src/main/java/pt/tecnico/mydrive/domain/FileSystem.java
@@ -18,26 +18,26 @@ public class FileSystem extends FileSystem_Base implements IXMLVisitable {
     private void init(String name) {
         setName(name);
     }
-    
-    public ArrayList<File> pathContent (ArrayList<String> path) throws UnknownPathException {
-    	return splitPath(path).showContent(); //FIXME
-    }
-    
-    public String fileContent (ArrayList<String> path) throws UnknownPathException {
-    	return splitPath(path).getContent(); //FIXME
-    }
-    
-    public void removeFile (ArrayList<String> path) throws UnknownPathException {
-    	splitPath(path).deleteFile(); 
-    }
-    
-    public File splitPath(ArrayList<String> path) throws UnknownPathException {
-    	File currentDir = getRootDir();
 
-    	for(String dir : path) 
-    		currentDir = currentDir.getContent(dir); //FIXME
-    	
-    	return currentDir;
+    public ArrayList<File> pathContent (ArrayList<String> path) throws UnknownPathException {
+        return null;//return splitPath(path).showContent(); //FIXME
+    }
+
+    public String fileContent (ArrayList<String> path) throws UnknownPathException {
+        return null; //return splitPath(path).getContent(); //FIXME
+    }
+
+    public void removeFile (ArrayList<String> path) throws UnknownPathException {
+        //FIXME splitPath(path).remove();
+    }
+
+    public File splitPath(ArrayList<String> path) throws UnknownPathException {
+        File currentDir = getRootDir();
+	/* FIXME
+        for(String dir : path)
+            currentDir = currentDir.getContent(dir); //FIXME
+        */
+        return currentDir;
     }
 
     @Override