2a5569a9af7fc2e8c6dcff58de101fd57f1037f8
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-03-13 04:16:35 +0000

    feat: finish PlainFile (for the 1st sprint)
    PlainFile will need more methods. However, everything needed for the 1st sprint is done.
    
    * implement getLines method. Note that you can still get the whole content of
      the PlainFile using the PlainFile_Base.getContent() method.
    * implement setLines method
    * add constructor with empty content
    
    closes #10

--- mydrive/src/main/java/pt/tecnico/mydrive/domain/PlainFile.java
+++ mydrive/src/main/java/pt/tecnico/mydrive/domain/PlainFile.java
@@ -3,8 +3,12 @@ import org.jdom2.Element;
 import pt.tecnico.mydrive.xml.IXMLVisitable;
 import pt.tecnico.mydrive.xml.IXMLVisitor;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PlainFile extends PlainFile_Base implements IXMLVisitable {
+    public static final String LINE_SEPARATOR = "\n";
 
     public PlainFile() {
         super();
@@ -13,12 +17,39 @@ public class PlainFile extends PlainFile_Base implements IXMLVisitable {
     public PlainFile(String name, byte perm, long id, String content) {
         init(name, perm, id, content);
     }
-    
-    protected void init(String name, byte perm, long id, String content){
-		super.init(name, perm, id);
-		setContent(content);
-	}
 
+    /** construct an empty PlainFile */
+    public PlainFile( String name, byte perm, long id ) {
+        init( name, perm, id, "" );
+    }
+
+    protected void init(String name, byte perm, long id, String content){
+        super.init(name, perm, id);
+        setContent(content);
+    }
+
+    /** Execute the file: each line is interpreted as "<app path> <args>*"
+     *  and each app is executed */
+    public void execute() {
+        // FIXME: not sure what this should return
+        // TODO: method not needed for the first sprint
+    }
+
+    /** @returns the content of the PlainFile as a List of lines */
+    public List<String> getLines() {
+        String content = getContent();
+        List<String> lines = Arrays.asList( content.split( LINE_SEPARATOR ) );
+        return lines;
+    }
+
+    /** sets the content of the PlainFile as a List of lines */
+    public void setLines( List<String> lines ) {
+        String content = "";
+        for( String line : lines ) {
+            content += line + LINE_SEPARATOR;
+        }
+        setContent( content );
+    }
 
     @Override
     public Element accept(IXMLVisitor visitor) {
