538d85443e8e88926105bd165a859c1a21804ab1
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-03-13 21:32:21 +0000

    refractor: change "users" to "user" in dml
    This results in a cleaner API, since now to add a user you invoke .addUser() and not .addUsers() on a FileSystem object

--- mydrive/src/main/java/pt/tecnico/mydrive/domain/FileSystem.java
+++ mydrive/src/main/java/pt/tecnico/mydrive/domain/FileSystem.java
@@ -31,7 +31,7 @@ public class FileSystem extends FileSystem_Base implements IXMLVisitable {
         Directory homeDir = new Directory("home", permission, numFiles);
         rootDir.addFile(homeDir);
         // Create Super User and respective directory: "/home/root"
-        addUsers(new User("root", "***", "Super User", (byte) 111101101));   // FIXME: construtor de user ja cria o seu diretorio
+        addUser(new User("root", "***", "Super User", (byte) 111101101));   // FIXME: construtor de user ja cria o seu diretorio
         numFiles+=1;
         Directory homeroot = new Directory("root", permission, numFiles);
         homeDir.addFile(homeroot);
