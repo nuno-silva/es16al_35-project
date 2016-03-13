0d12787f035806c132a380b0afba71274e7ea418
pmcarmo <pedro.m.carmo@hotmail.com>
2016-03-13 23:17:44 +0000

    fix: Changing UnknownPathException to be in conformity with other exceptions
    issues #19 and #12

--- mydrive/src/main/java/pt/tecnico/mydrive/exception/UnknownPathException.java
+++ mydrive/src/main/java/pt/tecnico/mydrive/exception/UnknownPathException.java
@@ -1,7 +1,18 @@
 package pt.tecnico.mydrive.exception;
 
-public class UnknownPathException extends Throwable {
-	public UnknownPathException() {
-		super("ERROR: Given path does not exist");
+public class UnknownPathException extends MydriveException {
+	private String _path;
+
+	public UnknownPathException( String path ) {
+		_path = path;
+	}
+
+	public String getPath() {
+		return _path;
+	}
+
+	@Override
+	public String getMessage() {
+		return "The given path '" + _path + "' is invalid";
 	}
 }
