223990de267265786909a6675250767d2825c7ec
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-03-14 09:37:37 +0000

    feat: implement FileSystem.createFileParents method
    closes #62

--- mydrive/src/main/java/pt/tecnico/mydrive/domain/FileSystem.java
+++ mydrive/src/main/java/pt/tecnico/mydrive/domain/FileSystem.java
@@ -2,8 +2,14 @@ package pt.tecnico.mydrive.domain;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
+
 import pt.tecnico.mydrive.exception.InvalidUsernameException;
 import pt.tecnico.mydrive.exception.UnknownPathException;
+import pt.tecnico.mydrive.exception.FilenameAlreadyExistsException;
+import pt.tecnico.mydrive.exception.FileNotFoundException;
+
+import pt.tecnico.mydrive.xml.IXMLVisitable;
+import pt.tecnico.mydrive.xml.IXMLVisitor;
 import pt.tecnico.mydrive.xml.XMLVisitor;
 
 import java.util.LinkedList;
@@ -52,6 +58,32 @@ public class FileSystem extends FileSystem_Base {
 		return root;
     }
 
+    /** Creates all parent directories for the given file path, if they
+     * don't exist. Does NOT create the given file.
+     * @returns the given file's parent Directory
+     */
+    public Directory createFileParents( String path ) {
+        Directory dir = getRootDir();
+        String currentPath = "";
+        String[] p = path.split("/");
+        int i = 1; // p[0] is ""
+        for( ; i < p.length - 1; i++ ) {
+            String dirName = p[i];
+            currentPath += "/" + dirName;
+            try {
+                File f = dir.getFileByName( dirName );
+                if( ! f.isCdAble() ) {
+                    // there's a file with that name
+                    throw new FilenameAlreadyExistsException( currentPath );
+                }
+                dir = (Directory) f;
+            } catch( FileNotFoundException e ) {
+                dir = createDirectory( dir, dirName, dir.getPerm() );
+            }
+        }
+        return dir;
+    }
+
     public Directory createDirectory(Directory parent, String name, byte permission) {
     	numFiles+=1;
     	Directory newDir = new Directory(parent, name, permission, numFiles);
@@ -63,7 +95,7 @@ public class FileSystem extends FileSystem_Base {
     	PlainFile newPlainFile = new PlainFile(parent, name, permission, numFiles);
     	return newPlainFile;
     }
-    
+
     public void createUser(String username, String password, String name) throws InvalidUsernameException {
     	addUser(new User(this, username, password, name, (byte) 00000000));
     }
@@ -84,7 +116,7 @@ public class FileSystem extends FileSystem_Base {
     	List<String> users = pathContent("/home");
     	Directory home = (Directory) getFile("/home");
     	numFiles+=1;
-    		
+
     	PlainFile readMe = new PlainFile(home, "README", (byte) 00000000, numFiles);
     	readMe.setLines(users);
     }
