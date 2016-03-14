nuno-silva/es16al_35-project

commit fadd191baf0e0d8f629a39cb1dc279f724586743

bug: File should add itself to its parent
closes #64

mydrive/src/main/java/pt/tecnico/mydrive/domain/File.java
@@ -19,14 +19,20 @@ public abstract class File extends File_Base implements IXMLVisitable {
         init(dir, name, perm, id);
     }
 
-    protected void init(Directory dir, String name, byte perm, long id){
-        setDirectory(dir);
+    protected void init(Directory parent, String name, byte perm, long id){
+        setDirectory(parent);
         setName(name);
         setId(id);
         setPerm(perm);
         //still need to add DateTime lastMod
     }
-    
+
+    @Override
+    public void setDirectory( Directory parent ) {
+        super.setDirectory( parent ); // not sure about this
+        parent.addFile(this);
+    }
+
     public boolean isCdAble() { return false; };
 
     public void remove() {

commit d999e93d6a4972837ca97749c4e6f81f2bab2844

bug: build almost fixed
* bug introduced in fadd191baf0e0d8f629a39cb1dc279f724586743 and b59b348919133256e79ce7ac5f27ba4c31bbddc4

mydrive/src/main/java/pt/tecnico/mydrive/domain/File.java
@@ -20,16 +20,19 @@ public abstract class File extends File_Base implements IXMLVisitable {
     }
 
     protected void init(Directory parent, String name, byte perm, long id){
-        setDirectory(parent);
         setName(name);
         setId(id);
         setPerm(perm);
+        setDirectory(parent); // must be called after setName!
         //still need to add DateTime lastMod
     }
 
     @Override
     public void setDirectory( Directory parent ) {
-        super.setDirectory( parent ); // not sure about this
+        if( parent == null ) {
+            super.setDirectory( parent );
+            return;
+        }
         parent.addFile(this);
     }
 

mydrive/src/main/java/pt/tecnico/mydrive/domain/User.java
@@ -38,7 +38,10 @@ public class User extends User_Base implements IXMLVisitable {
 
     @Override
     public void setFs( FileSystem fs ) {
-        super.setFs(fs); // not sure about this
+        if( fs == null ) {
+            super.setFs(fs);
+            return;
+        }
         fs.addUser(this);
     }
 