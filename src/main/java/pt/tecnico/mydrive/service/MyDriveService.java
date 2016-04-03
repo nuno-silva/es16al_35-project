4b2077e93e103e488fe62f88f2ab3d0f64c8429d
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-04-03 13:24:19 +0100

    feat: add MyDriveService
    closes #121

--- src/main/java/pt/tecnico/mydrive/service/MyDriveService.java
+++ src/main/java/pt/tecnico/mydrive/service/MyDriveService.java
@@ -1,7 +1,42 @@
 package pt.tecnico.mydrive.service;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.mydrive.domain.FileSystem;
+import pt.tecnico.mydrive.domain.User;
+import pt.tecnico.mydrive.exception.InvalidUsernameException;
+import pt.tecnico.mydrive.exception.MydriveException;
+
+import java.util.Optional;
+
 /**
- * Created by iluxonchik on 03-04-2016.
+ * Abstract base class from which all of the services inherit.
  */
-public class MyDriveService {
+public abstract class MyDriveService {
+    protected final static Logger logger = LogManager.getRootLogger();
+
+    static FileSystem getFileSystem()  {
+        return FileSystem.getInstance();
+    }
+
+    static User getUser(String username) {
+        if (username == null) {
+            throw new InvalidUsernameException("Username cannot be \"null\".");
+        }
+        Optional<User> opt = getFileSystem().getUserByUsername(username);
+
+        if (!opt.isPresent()) {
+            throw new InvalidUsernameException("User with username " + username + "does not exist.");
+        }
+
+        return opt.get();
+    }
+
+    @Atomic
+    public final void execute() throws MydriveException {
+        dispatch();
+    }
+
+    protected abstract void dispatch() throws MydriveException;
 }
