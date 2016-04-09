95edb89fc2bec255209808ef57a9e7ef57dc2523
goncalo1995 <goncalo.ribeiro.pereira@gmail.com>
2016-04-09 05:19:36 +0100

    test: ChangeDirectoryTest [WIP]
    * created dummy ChangeDirectoryService
    
    related to #91 #120

---
+++ src/main/java/pt/tecnico/mydrive/service/ChangeDirectoryService.java
@@ -0,0 +1,17 @@
+package pt.tecnico.mydrive.service;
+
+import pt.tecnico.mydrive.exception.MydriveException;
+
+public class ChangeDirectoryService extends MyDriveService {
+	
+	public ChangeDirectoryService(long token,String path) {
+        
+    }
+	
+	@Override
+	protected void dispatch() throws MydriveException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
