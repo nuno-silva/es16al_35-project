39f5b07663623458a35e4ddb01b728cd0da77b9a
goncalo1995 <goncalo.ribeiro.pereira@gmail.com>
2016-04-14 23:21:08 +0100

    included exception

---
+++ src/main/java/pt/tecnico/mydrive/exception/ReadDirectoryException.java
@@ -0,0 +1,12 @@
+package pt.tecnico.mydrive.exception;
+
+public class ReadDirectoryException extends MydriveException {
+	
+    public ReadDirectoryException() {
+        super();
+    }
+
+    public ReadDirectoryException(String msg) {
+        super(msg);
+    }
+}
