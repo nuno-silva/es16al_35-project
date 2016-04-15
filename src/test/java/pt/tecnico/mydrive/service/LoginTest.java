a4d3dec2a5cefff761d4e605c097ba8f3dba5622
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-04-15 09:33:47 +0100

    feat: add more tests to Login Service Test
    * testing Login for the root user
    
    took <1/2h
    
    related to #124

--- src/test/java/pt/tecnico/mydrive/service/LoginTest.java
+++ src/test/java/pt/tecnico/mydrive/service/LoginTest.java
@@ -22,6 +22,8 @@ public class LoginTest extends AbstractServiceTest {
     private final String valid_password = "that's right dude";
     private final String invalid_username = "no";
     private final String invalid_password = "I'm wrong";
+    private final String root_username    = "root";
+    private final String root_password    = "***";
     private User valid_user;
     private FileSystem fs;
 
@@ -64,9 +66,30 @@ public class LoginTest extends AbstractServiceTest {
         assertEquals("Session 2 not created", token2, s2.getToken());
     }
 
+    @Test
+    public void successWithRootLogin() {
+        LoginService service = new LoginService(root_username, root_password);
+        service.execute();
+
+        long token = service.result();
+
+        Session s = fs.getSession(token);
+        User root = fs.getUser(root_username);
+
+        assertEquals("Session not created", token, s.getToken());
+        assertEquals("Session with wrong user", root, s.getUser());
+        assertFalse("Session expired", s.isExpired());
+    }
+
     @Test(expected = WrongPasswordException.class)
     public void successWithWrongPassword() {
         LoginService service = new LoginService(valid_username, invalid_password);
+        service.execute();
+    }
+
+    @Test(expected = WrongPasswordException.class)
+    public void successWithWrongRootPassword() {
+        LoginService service = new LoginService(root_username, root_password + invalid_password);
         service.execute();
     }
 
