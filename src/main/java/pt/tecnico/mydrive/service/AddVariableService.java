8184296ef6c3c13c471058b6d34bd11f0ea30e59
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-05-02 04:03:08 +0100

    feat: implement AddVariableService
    took 1/2h
    
    closes #160

--- src/main/java/pt/tecnico/mydrive/service/AddVariableService.java
+++ src/main/java/pt/tecnico/mydrive/service/AddVariableService.java
@@ -1,27 +1,24 @@
 package pt.tecnico.mydrive.service;
 
-import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.domain.FileSystem;
+import pt.tecnico.mydrive.domain.Session;
+import pt.tecnico.mydrive.domain.Variable;
 
 public class AddVariableService extends MyDriveService {
-
     private String _name;
     private String _value;
     private long _token;
 
     public AddVariableService(long token, String name, String value) {
-      _name=name;
-      _value=value;
-      _token=token;
-    }
-
-    public AddVariableService(long token, String name) {
-      _name=name;
-      _value="";
-      _token=token;
+        _name=name;
+        _value=value;
+        _token=token;
     }
 
     @Override
-    protected void dispatch(){
-      /* TODO: Does stuff */
+    protected void dispatch() {
+        FileSystem fs = FileSystem.getInstance();
+        Session s = fs.getSession(_token);
+        new Variable(s, _name, _value);
     }
 }
