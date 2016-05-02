520d7a2433640bc958098ff1b14136cd19a65465
Gisson <jorge.heleno@tecnico.ulisboa.pt>
2016-05-02 02:28:54 +0100

    feat: Add variable Test
    Added file for test adding variable
    
    Refers to #161

---
+++ src/test/java/pt/tecnico/mydrive/service/AddVariableTest.java
@@ -0,0 +1,22 @@
+package pt.tecnico.mydrive.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import pt.tecnico.mydrive.domain.*;
+
+public class AddVariableTest extends AbstractServiceTest {
+
+	@Override
+	protected void populate() {
+    /* TODO: Add stuff */
+  }
+
+    @Test
+    public void successUser() {
+      /* TODO: Add stuff */
+    }
+
+}
