bf2c1064e5af7ffb3b9037d3649aa2b65c53e4bb
Gisson <jorge.heleno@tecnico.ulisboa.pt>
2016-05-02 02:26:01 +0100

    refactor: Added pretty message for List error
    Added a proper error handling when user isn't logged yet.
    
    Refers to #154

--- src/main/java/pt/tecnico/mydrive/presentation/List.java
+++ src/main/java/pt/tecnico/mydrive/presentation/List.java
@@ -1,32 +1,42 @@
 package pt.tecnico.mydrive.presentation;
 
-import pt.tecnico.mydrive.service.ListDirectoryService;
+/*Exceptions*/
+import pt.tecnico.mydrive.exception.InvalidTokenException;
 import java.lang.UnsupportedOperationException;
+
+/* Services */
+import pt.tecnico.mydrive.service.ListDirectoryService;
 import pt.tecnico.mydrive.service.dto.FileDto;
-import java.nio.charset.StandardCharsets;
+
+/*Other stuff*/
+
 
 
 public class List extends MydriveCommand {
 
-    public List(Shell sh) { super(sh, "List", "List a directory"); }
+    public List(Shell sh) { super(sh, "list", "List a directory"); }
     public void execute(String[] args) {
       ListDirectoryService lds;
       /* Check if argument length is at least the size of the obligatory */
-      if( args.length==0 ||  args[0].trim()=="."){
-        MydriveShell mds = (MydriveShell) shell();
-        lds = new ListDirectoryService(mds.getToken());
-        lds.execute();
-      }
-      if( args.length>1 ){
-        throw new RuntimeException("USAGE: "+name()+"list [path]");
-      }
-      else{
-        MydriveShell mds = (MydriveShell) shell();
-        lds = new ListDirectoryService(mds.getToken(),args[0]);
-        lds.execute();
-      }
-      for( FileDto f : lds.result() ){
-        shell().println(f.getId()+" "+f.getName()+" "+ f.getPermissions()+" "+f.getlastMod());
+      try{
+        if( args.length==0 ||  args[0].trim()=="."){
+          MydriveShell mds = (MydriveShell) shell();
+          lds = new ListDirectoryService(mds.getToken());
+          lds.execute();
+        }
+        if( args.length>1 ){
+          throw new RuntimeException("USAGE: "+name()+"list [path]");
+        }
+        else{
+          MydriveShell mds = (MydriveShell) shell();
+          lds = new ListDirectoryService(mds.getToken(),args[0]);
+          lds.execute();
+        }
+        for( FileDto f : lds.result() ){
+          shell().println(f.getId()+" "+f.getName()+" "+ f.getPermissions()+" "+f.getlastMod());
+        }
+      }catch (InvalidTokenException e){
+        shell().println("You need to login first!");
       }
     }
 }
