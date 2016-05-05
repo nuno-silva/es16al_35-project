b5676768881624aad4149db953238d76b912fa8b
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-05-05 13:51:16 +0100

    feat: AddVariableService returns all defined variables
    @gisson proceed with #161
    
    took <1/2h
    
    closes #160

--- src/main/java/pt/tecnico/mydrive/service/AddVariableService.java
+++ src/main/java/pt/tecnico/mydrive/service/AddVariableService.java
@@ -1,5 +1,7 @@
 package pt.tecnico.mydrive.service;
 
+import java.util.Map;
+import java.util.HashMap;
 import pt.tecnico.mydrive.domain.FileSystem;
 import pt.tecnico.mydrive.domain.Session;
 import pt.tecnico.mydrive.domain.Variable;
@@ -8,6 +10,7 @@ public class AddVariableService extends MyDriveService {
     private String _name;
     private String _value;
     private long _token;
+    private Map<String, String> _vars;
 
     public AddVariableService(long token, String name, String value) {
         _name=name;
@@ -20,5 +23,16 @@ public class AddVariableService extends MyDriveService {
         FileSystem fs = FileSystem.getInstance();
         Session s = fs.getSession(_token);
         new Variable(s, _name, _value);
+
+        _vars = new HashMap<String, String>();
+
+        for(Variable v : s.getVariableSet()) {
+            _vars.put( v.getName(), v.getValue() );
+        }
+    }
+
+    public Map<String, String> result() {
+        assertExecuted();
+        return _vars;
     }
 }
