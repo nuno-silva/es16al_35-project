c98626ccfb3895ff21b50d433c4e1f28c7276ba4
bernardobranco <bernardo.branco1@gmail.com>
2016-05-08 19:49:34 +0100

    feat: Write command implemented
    WriteFileService has now to be modified to be able to receive path names and not just filenames.
    
    fix: LoginCommand was also altered, since it can receive or not a password
    
    LoginService has to be modified in order to be able to login guest users.
    
    refers #156 #152

--- src/main/java/pt/tecnico/mydrive/presentation/LoginCommand.java
+++ src/main/java/pt/tecnico/mydrive/presentation/LoginCommand.java
@@ -9,14 +9,16 @@ public class LoginCommand extends Command{
 	}
 	@Override
 	void execute(String[] args) {
-		// TODO Auto-generated method stub
-		if (args.length != 2)
+		if (args.length == 0 || args.length > 2)
 		    throw new RuntimeException("USAGE: "+name()+" login <username> [<password>]");
-		else {
+		if (args.length == 2) {
 	          LoginService ls = new LoginService(args[0], args[1]);
 	          ls.execute();
 	          MydriveShell mds = (MydriveShell) shell();
 	          mds.setToken(ls.result());
+		}
+		else {
+			// TODO: call loginService with only one argument (userame) for logging in the guestUser
 		}
 	}
 }
---
+++ src/main/java/pt/tecnico/mydrive/presentation/WriteCommand.java
@@ -0,0 +1,29 @@
+package pt.tecnico.mydrive.presentation;
+
+import pt.tecnico.mydrive.exception.InvalidTokenException;
+import pt.tecnico.mydrive.service.ListDirectoryService;
+import pt.tecnico.mydrive.service.WriteFileService;
+import pt.tecnico.mydrive.service.dto.FileDto;
+
+public class WriteCommand extends Command {
+
+	public WriteCommand(Shell sh) {
+		super(sh, "write", "writes text to a file");
+	}
+	@Override
+	void execute(String[] args) {
+		WriteFileService wfs;
+		if (args.length != 2)
+		    throw new RuntimeException("USAGE: "+name()+" update <path> <text>");
+		else {
+			try{
+				MydriveShell mds = (MydriveShell) shell();
+				wfs = new WriteFileService(mds.getToken(), args[0], args[1]);
+				wfs.execute();
+			} catch (InvalidTokenException e){
+				shell().println("You need to login first!");
+			}
+		}
+	}
+
+}
