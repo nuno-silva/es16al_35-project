d0637b854fab141829433ea50f396fc73f20b320
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-05-09 06:26:39 +0100

    feat: Login Guest User on Shell startup
    @gisson, sorry, I need this to test my command
    
    took < 1/2h
    
    related to #163

--- src/main/java/pt/tecnico/mydrive/presentation/MydriveShell.java
+++ src/main/java/pt/tecnico/mydrive/presentation/MydriveShell.java
@@ -1,14 +1,23 @@
 package pt.tecnico.mydrive.presentation;
 
-
+import pt.tecnico.mydrive.service.LoginService;
 
 public class MydriveShell extends Shell {
+  private static final String INITIAL_USER = "nobody";
 
   private long _token;
 
   public static void main(String[] args) throws Exception {
     MydriveShell sh = new MydriveShell();
     sh.execute();
+  }
+
+  @Override
+  public void execute() throws Exception {
+    LoginService login = new LoginService(INITIAL_USER, "");
+    login.execute();
+    _token = login.result();
+    super.execute();
   }
 
   public MydriveShell() {
