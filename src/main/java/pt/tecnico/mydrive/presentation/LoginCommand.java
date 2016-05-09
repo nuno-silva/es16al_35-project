70f06b14e1fbb17c95feae9e8491a1400018d779
bernardobranco <bernardo.branco1@gmail.com>
2016-05-09 13:12:30 +0100

    feat:
    Login command now is able to log in guest users (password is left empty)
    
    closes #152

--- src/main/java/pt/tecnico/mydrive/presentation/LoginCommand.java
+++ src/main/java/pt/tecnico/mydrive/presentation/LoginCommand.java
@@ -9,16 +9,15 @@ public class LoginCommand extends Command{
 	}
 	@Override
 	void execute(String[] args) {
+		LoginService ls;
 		if (args.length == 0 || args.length > 2)
 		    throw new RuntimeException("USAGE: "+name()+" login <username> [<password>]");
-		if (args.length == 2) {
-	          LoginService ls = new LoginService(args[0], args[1]);
-	          ls.execute();
-	          MydriveShell mds = (MydriveShell) shell();
-	          mds.setToken(ls.result());
-		}
-		else {
-			// TODO: call loginService with only one argument (userame) for logging in the guestUser
-		}
+		if (args.length == 2)
+	          ls = new LoginService(args[0], args[1]);
+		else
+	          ls = new LoginService(args[0], "");
+        ls.execute();
+        MydriveShell mds = (MydriveShell) shell();
+        mds.setToken(ls.result());
 	}
 }
